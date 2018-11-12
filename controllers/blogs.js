87969c018f74345ad8cd0081aea792d0c8b69a96
Laura Tuomola <laura.tuomola@helsinki.fi>
2018-11-12 20:46:53 +0200

    exercise 18 and 19 done: valid token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,54 +1,76 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-  .find({})
-  .populate('user', {username: 1, name: 1})
+    .find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(blogs.map(Blog.format))
 
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  try {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-  if (body.title === undefined) {
-    return response.status(400).json({ error: 'title missing' })
-  }
-  if (body.title === undefined && body.url === undefined) {
-    return response.status(400).json({ error: 'title and url missing' })
-  }
-  if (body.likes === undefined) {
-    const user = await User.findById(body.userId)
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: 0,
-      user: user._id
-    })
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.json(Blog.format(blog))
-  } else {
-    const user = await User.findById(body.userId)
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      user: user._id
-    })
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.json(Blog.format(blog))
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    if (body.title === undefined) {
+      return response.status(400).json({ error: 'title missing' })
+    }
+    if (body.title === undefined && body.url === undefined) {
+      return response.status(400).json({ error: 'title and url missing' })
+    }
+    if (body.likes === undefined) {
+      const user = await User.findById(decodedToken.id)
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: 0,
+        user: user._id
+      })
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.json(Blog.format(blog))
+    } else {
+      const user = await User.findById(decodedToken.id)
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+      })
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.json(Blog.format(blog))
+    }} catch (exception) {
+      if (exception.name === 'JsonWebTokenError') {
+        response.status(401).json({ error: exception.message })
+      } else {
+        console.log(exception)
+        response.status(500).json({ error: 'something went wrong...' })
+      }
   }
 
-})
+  })
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 
 
@@ -19,6 +20,7 @@ app.use(express.static('build'))
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
